b19ef815f7508ce37ed225e0c623e502d79abcc5
Khiaotic <hurtmyfeelings966@gmail.com>
2023-05-29 19:33:49 -0400

    html elements added

---
+++ khiaoticwave/src/components/TOP.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TermsOfProduct = () =>
+{
+    return (
+        <div id="popup-modal">
+<div class="relative w-full"> 
+<div> 
+    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover: bg-gray-200 dark:hover: text-gray-900 rounded-lg text sm p-1.5 ml-auto inline-flex intems-center dark:hover:bg-gray-800 dark:hover:text-white"> 
+    
+    </button>
+</div>
+</div>
+        </div>
+    )
+}
